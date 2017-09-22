124e92e09ec92953dcbc55a57d7350b962fb4d36
xpeper <xpepermint@gmail.com>
2017-09-22 13:11:43 +0200

    dependencies

--- dist/handlers/index.js
+++ dist/handlers/index.js
@@ -1,4 +1,5 @@
 "use strict";
+exports.__esModule = true;
 var block_1 = require("./block");
 exports.block = block_1.block;
 var mongo_uniqueness_1 = require("./mongo-uniqueness");
--- dist/handlers/mongo-uniqueness.js
+++ dist/handlers/mongo-uniqueness.js
@@ -1,4 +1,5 @@
 "use strict";
+exports.__esModule = true;
 function mongoUniqueness(error, options) {
     if (options === void 0) { options = {}; }
     if (!error || !options)
